fad738bb1dc24a2e6242524632d1362dccfae0b9
Daniel Sundberg <sumpan@gmail.com>
2020-10-28 14:55:39 +0100

    Display loader when saving data in EnableTelemetryForm

--- src/Views/EnableTelemetryForm.tsx
+++ src/Views/EnableTelemetryForm.tsx
@@ -7,6 +7,7 @@ import { register as registerDevice } from '../Model/DeviceUtils';
 
 interface EnableTelemetryFormState {
     hasSelected: boolean;
+    isLoading: boolean;
 }
 
 // Change this if we want to show message one more time
@@ -22,21 +23,27 @@ class EnableTelemetryForm extends React.Component<RootStore, EnableTelemetryForm
       super(props);
       this.state = {
         hasSelected: false,
+        isLoading: false
       };
     }
 
     async clickedYes(self: any) { // tslint:disable-line
-      localStorage.setItem(hasSeentelemetrySettingStr, "true");
-      console.log(`Save settings, enable telemetry: ${true}, will auto generate device name.`); // tslint:disable-line
-      const rsp = await registerDevice(
-          this.props.containerAppCallbacks.url, 
-          this.props.containerAppCallbacks.apiKey, 
-          true
-      );
-      console.log("Response: ", rsp.errorMessage || "ok"); // tslint:disable-line
-      self.setState({
-          hasSelected: true
-      });
+        self.setState({
+            isLoading: true
+        });
+        
+        console.log(`Save settings, enable telemetry: ${true}, will auto generate device name.`); // tslint:disable-line
+        const rsp = await registerDevice(
+            this.props.containerAppCallbacks.url, 
+            this.props.containerAppCallbacks.apiKey, 
+            true
+        );
+        
+        console.log("Response from registerTelemetry: ", rsp.errorMessage || "ok"); // tslint:disable-line
+        localStorage.setItem(hasSeentelemetrySettingStr, "true");
+        self.setState({
+            hasSelected: true
+        });
     }
 
     clickedNo(self: any) { // tslint:disable-line
@@ -56,6 +63,23 @@ class EnableTelemetryForm extends React.Component<RootStore, EnableTelemetryForm
         if (hasSeenTelemetryMessage === "true" || telemetryEnabled) {
             return (<Redirect to="/blogs" />);
         } 
+
+        const thumbsUpButtonOrLoader = this.state.isLoading ?
+            <button 
+                className="ui large primary loading fluid button"
+                style={this.props.theme.activeButton()}
+            >
+                Loading
+            </button> : 
+            <div 
+                className="ui fluid large primary submit button" 
+                style={this.props.theme.activeButton()}
+                // tslint:disable-next-line
+                onClick={(ev: any) => this.clickedYes(this)}
+            >
+                <i className="thumbs up icon" /> YES, I'm in!
+            </div>;
+
         return (
             <div className="ui grid middle center container">
                 <div className="row" />
@@ -68,14 +92,7 @@ class EnableTelemetryForm extends React.Component<RootStore, EnableTelemetryForm
                     <TelemetryInfo style={this.props.theme.infoMessage()}/>
 
                     {/* Yes I'm in button */}
-                    <div 
-                        className="ui fluid large primary submit button" 
-                        style={this.props.theme.activeButton()}
-                        // tslint:disable-next-line
-                        onClick={(ev: any) => this.clickedYes(this)}
-                    >
-                        <i className="thumbs up icon" /> YES, I'm in!
-                    </div>
+                    {thumbsUpButtonOrLoader}
                     <br/>
 
                     {/* No, not right now button */}
